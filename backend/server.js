5ef9c12903973fe5133a13d761a4ddd7b261d7f7
Ayesha Ansari <ayeshansari124@gmail.com>
2026-06-28 22:38:23 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://track-it-rho-blush.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://trackit-z090.onrender.com/",
+  baseURL: "https://trackit-z090.onrender.com/api",
 });
 
 export default api;
